435741b8d1c6d5baacc8c8d95d61de8969fade9b
idietmoran <idietmoran@gmail.com>
2016-02-18 02:12:58 -0500

    1

--- app/model/user.js
+++ app/model/user.js
@@ -1,13 +1,29 @@
 // app/model/user.js
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
     username: String,
     email: String,
-    password: Hash,
+    password: String,
     polls: Number
 });
+
+// methods
+// ================
+
+// generate a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if the password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password
+    this.password);
+};
+
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 //port
 var port = process.env.PORT || 8080;
@@ -16,6 +22,10 @@ mongoose.connect('mongodb://localhost:27017');
 
 // confiure app to use bodyParser()
 // this will let us get the data from a POST
+app.use(morgan('dev'));  // logs every request to console
+app.use(cookieParser()); //read cookies
+app.use(bodyParser());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -46,7 +56,6 @@ router.route('/polldata/:id')
     .get(function(req, res) {
         res.send('data');
     });
-
 // set the static files location
 // ---------------------------------
 app.use(express.static(__dirname + '/public')); 
